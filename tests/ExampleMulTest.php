e4e020123f7eacc05d1343476b75b16413bc5875
Oleg Grigoriev <go.vasac@gmail.com>
2023-03-10 20:03:54 +0400

    Up PHPUnit to 10

--- tests/ExampleMulTest.php
+++ tests/ExampleMulTest.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace axy\pkg\tpl\tests;
 
 use axy\pkg\tpl\ExampleMul;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 /**
  * no-copy-to-pkg
  */
 class ExampleMulTest extends BaseTestCase
 {
-    /** @dataProvider providerMul */
+    #[DataProvider('providerMul')]
     public function testMul(int $first, int $second, int $expected): void
     {
         $this->assertSame($expected, (new ExampleMul($first))->mul($second));
     }
 
-    public function providerMul(): array
+    public static function providerMul(): array
     {
         return [
             '2x2=4' => [2, 2, 4],
